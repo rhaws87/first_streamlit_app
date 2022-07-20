7e92d4d507b09a6a24ef2b3757ca78aab9f18a12
rhaws87 <51512708+rhaws87@users.noreply.github.com>
2022-07-20 08:25:09 -0500

    Update streamlit_app.py
    cleaned up code to match "more functions and some buttons"

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,12 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-streamlit.stop()  
+
 #Allow end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     mycur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding ", new_fruit
+    return "Thanks for adding " + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
@@ -69,10 +69,9 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-
+streamlit.stop()  
 
 #my_cur = my_cnx.cursor()
 #my_cur.execute("select * from fruit_load_list")
 #mycur.execute("insert into fruit_load_list values ('from streamlit')")
+#streamlit.write('Thanks for adding ', add_my_fruit)
